982e399e41405f9c9f8bcb7960139637470b52fd
tom <tommy.dumont@uqtr.ca>
2022-03-11 11:43:12 -0500

    Initial commit

---
+++ src/com/company/Lc_13.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Lc_13 {
+
+        public int romanToInt(String s) {
+            int[] containNumbers = new int[s.length()];
+            int finalNumber =0;
+            for (int i =0 ; i< s.length(); ++i) {
+                switch (s.charAt(i)) {
+                    case 'I': containNumbers[i] = 1; break;
+                    case 'V': containNumbers[i] = 5;break;
+                    case 'X': containNumbers[i] =10;break;
+                    case 'L': containNumbers[i] = 50;break;
+                    case 'C': containNumbers[i] = 100;break;
+                    case 'D': containNumbers[i] = 500;break;
+                    case 'M': containNumbers[i] = 1000;break;
+                }
+            }
+
+            for(int i =0; i < containNumbers.length; i++) {
+
+                if (containNumbers[i] < containNumbers[i+1]){
+                    finalNumber = containNumbers[i+1] - containNumbers[i];
+                    i++;
+                }else {
+                    finalNumber += containNumbers[i];
+                }
+
+            }
+            return finalNumber;
+        }
+    }
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Lc_13 lc_13 = new Lc_13();
+        System.out.println(lc_13.romanToInt("IV"));
+    }
+}
