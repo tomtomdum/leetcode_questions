372bb7239a482a042a6b5f445b793d50f4b34b8f
tom <tommy.dumont@uqtr.ca>
2022-03-11 16:30:25 -0500

    Lc_1 working

---
+++ src/com/company/Lc_1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Lc_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for (int i =0; i < nums.length; i++){
+            for (int j =0; j < nums.length; j ++){
+                if( i != j && nums[i] + nums[j] == target){
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
+                }
+            }
+        } return null;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.time.Duration;
+import java.util.Arrays;
 
 public class Main {
 
@@ -10,6 +10,12 @@ public class Main {
         Lc_13 lc_13 = new Lc_13();
         System.out.println(lc_13.romanToInt("MCMXCIV"));
         long stopTime = System.nanoTime();
+        System.out.println(stopTime - startTime);
+
+        startTime = System.nanoTime();
+        Lc_1 lc_1 = new Lc_1();
+        System.out.println(Arrays.toString(lc_1.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        stopTime = System.nanoTime();
         System.out.println(stopTime - startTime);
 
     }
