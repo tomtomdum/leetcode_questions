234df2db5a78d1ecb949e109bad11b4f2cee0a09
tom <tommy.dumont@uqtr.ca>
2022-03-11 11:49:20 -0500

    Lc_13 fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
+import java.time.Duration;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        long startTime = System.nanoTime();
         Lc_13 lc_13 = new Lc_13();
-        System.out.println(lc_13.romanToInt("IV"));
+        System.out.println(lc_13.romanToInt("MCMXCIV"));
+        long stopTime = System.nanoTime();
+        System.out.println(stopTime - startTime);
+
     }
 }
